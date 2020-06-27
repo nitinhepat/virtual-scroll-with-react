d01b83ebdec93d071ce08fc30dbce88417a59afe
Nitin Hepat <nitinhepat3@gmail.com>
2020-06-27 19:42:39 +0530

    refactored virtual scroll

--- src/components/virtualScroll/VirtualScroll.js
+++ src/components/virtualScroll/VirtualScroll.js
@@ -1,10 +1,8 @@
-import React, { useRef, useState,useEffect } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 
 const VirtualScroll = (props) => {
   const totalHeight = +props.rowHeight * +props.totalElements + 'px';
-  const [scrollTop,setScrollTop] = useState(0);
-  // const [transformValue, setTransformState] = useState(`translateY(0)`);
-  // const [visibleItems, setVisibleItem] = useState(props.items.slice(0, props.visibleItemsLength))
+  const [scrollTop, setScrollTop] = useState(0);
 
   useEffect(() => {
     scrollEle.current.addEventListener("scroll", scroll);
@@ -13,23 +11,29 @@ const VirtualScroll = (props) => {
     }
   })
   const scrollEle = useRef();
-  let startNodeele = Math.max(0,Math.floor(scrollTop / +props.rowHeight));
+  let startNodeele = Math.max(0, Math.floor(scrollTop / +props.rowHeight));
 
   let visibleItems = props.items.slice(startNodeele, startNodeele + props.visibleItemsLength);
-  let transformValue = `translateY(${startNodeele*+props.rowHeight}px)`;
-  console.log('rendering',Math.random());
+  let transformValue = `translateY(${startNodeele * +props.rowHeight}px)`;
+  console.log('rendering', Math.random());
 
   const scroll = () => {
     setScrollTop(scrollEle.current.scrollTop)
-   
+
   }
+  const scrollContainerStyle = {
+    height: props.height,
+    overflowY: "scroll"
+  }
+  const totalHeightStyle = { height: totalHeight };
+  const mainContainerStyle = { transform: transformValue }
   return (
-    <div className="scrollContainer" ref={scrollEle} style={{ height: props.height, overflowY: "scroll" }}>
-      <div  style={{ height: totalHeight}}>
-      <div className="main-container" style={{transform: transformValue }}>
-        {visibleItems}
+    <div className="scrollContainer" ref={scrollEle} style={scrollContainerStyle}>
+      <div style={totalHeightStyle}>
+        <div className="main-container" style={mainContainerStyle}>
+          {visibleItems}
+        </div>
       </div>
-    </div>
     </div>
   )
 }
